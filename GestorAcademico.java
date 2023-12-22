d5146a5a724be101c1bf36b5173ba7d553d975af
Felipe <andresfppy@gmail.com>
2023-12-22 14:26:42 -0500

    Implementación de funciones relacionadas con estudiantes y cursos

--- GestorAcademico.java
+++ GestorAcademico.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class GestorAcademico {
+public class GestorAcademico  implements ServiciosAcademicosI{
     private ArrayList<Estudiante> estudiantes = new ArrayList<>();
     private ArrayList<Curso> cursos = new ArrayList<>();
     private HashMap<Curso, ArrayList<Estudiante>> estudiantesPorCurso;
@@ -47,4 +47,80 @@ public class GestorAcademico {
                 ", estudiantesPorCurso=" + estudiantesPorCurso +
                 '}';
     }
+
+    @Override
+    public void matriculaEstudiante(Estudiante estudiante) {
+        boolean estudianteMatriculado = false;
+
+        for (Estudiante estudiant : estudiantes){
+            if(estudiante.getId() == estudiant.getId()){
+                estudianteMatriculado = true;
+                break;
+            }
+        }
+
+        if(estudianteMatriculado == false){
+            estudiantes.add(estudiante);
+        }
+    }
+
+    @Override
+    public void agregarCurso(Curso curso) {
+        boolean cursoMatriculado = false;
+
+        for (Curso curs : cursos){
+            if(curso.getId() == curso.getId()){
+                cursoMatriculado = true;
+                break;
+            }
+        }
+
+        if(cursoMatriculado == false){
+            cursos.add(curso);
+        }
+    }
+
+    @Override
+    public void inscribirEstudianteCurso(Estudiante estudiante, int idCurso) throws EstudianteYaInscritoException {
+        Curso cursoElegido = null;
+
+        for (Curso curso : cursos) {
+            if (curso.getId() == idCurso) {
+                cursoElegido = curso;
+                break;
+            }
+        }
+
+        if (cursoElegido != null && estudiantesPorCurso.containsKey(cursoElegido)) {
+            ArrayList<Estudiante> estudiantesInscritos = estudiantesPorCurso.get(cursoElegido);
+
+            if (estudiantesInscritos.contains(estudiante) == false) {
+                estudiantesInscritos.add(estudiante);
+            } else {
+                throw new EstudianteYaInscritoException("El estudiante ya está inscrito");
+            }
+        }
+    }
+
+    @Override
+    public void desinscribirEstudianteCurso(int idEstudiante, int idCurso) throws EstudianteNoInscritoEnCursoException {
+        Curso cursoElegido = null;
+
+        // Buscar el curso con el id proporcionado
+        for (Curso curso : cursos) {
+            if (curso.getId() == idCurso) {
+                cursoElegido = curso;
+                break;
+            }
+        }
+
+        if (cursoElegido != null && estudiantesPorCurso.containsKey(cursoElegido)) {
+            ArrayList<Estudiante> estudiantesInscritos = estudiantesPorCurso.get(cursoElegido);
+
+            if (!estudiantesInscritos.removeIf(inscrito -> inscrito.getId() == idEstudiante)) {
+                throw new EstudianteNoInscritoEnCursoException("El estudiante no está inscrito.");
+            }
+        }
+    }
+
 }
