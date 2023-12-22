186878911ba0539a8913f68c51b6b848798d6608
Felipe <andresfppy@gmail.com>
2023-12-22 10:23:55 -0500

    Implementación de Interfaz ServiciosAcademicosI

---
+++ ServiciosAcademicosI.java
@@ -0,0 +1,8 @@
+public interface ServiciosAcademicosI {
+
+    void matriculaEstudiante(Estudiante estudiante);
+    void agregarCurso(Curso curso);
+    void inscribirEstudianteCurso(Estudiante estudiante, int idCurso) throws EstudianteYaInscritoException;
+    void desinscribirEstudianteCurso(int idEstudiante, int idCurso) throws EstudianteNoInscritoEnCursoException;
+
+}
