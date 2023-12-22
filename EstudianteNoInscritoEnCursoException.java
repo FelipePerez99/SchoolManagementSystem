22e4088a1084f7da2a3ef678b66cbf60e21217d5
Felipe <andresfppy@gmail.com>
2023-12-22 10:33:25 -0500

    Implementación de las clases de excepciones

--- EstudianteNoInscritoEnCursoException.java
+++ EstudianteNoInscritoEnCursoException.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class EstudianteNoInscritoEnCursoException {
+public class EstudianteNoInscritoEnCursoException extends Exception {
+    public EstudianteNoInscritoEnCursoException(String mensaje){
+        super(mensaje);
+    }
 }
--- EstudianteYaInscritoException.java
+++ EstudianteYaInscritoException.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class EstudianteYaInscritoException {
+public class EstudianteYaInscritoException extends Exception {
+    public EstudianteYaInscritoException(String mensaje){
+        super(mensaje);
+    }
 }
